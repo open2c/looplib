c8e6dc68f0aa365544b0742ae2b7d2fc7321bea9
Hadi Rahmaninejad <rahmanin@discovery2.hpc.usc.edu>
2025-02-09 18:32:25 -0800

    adding a new file to include functions that relies on barriers, including FRiP

---
+++ looplib/barriertools.py
@@ -0,0 +1,18 @@
+def peak_positions(boundary_list, window_sizes=[1]):
+    """
+    Calculate peak positions based on a boundary_list within window_sizes.
+
+    Args:
+        boundary_list (list): List of boundary values.
+        window_sizes (list, optional): List of window sizes. Defaults to [1].
+
+    Returns:
+        np.ndarray: Array containing peak positions.
+    """
+    peak_monomers = np.array([])
+
+    for i in window_sizes:
+        inds_to_add = [boundary + i for boundary in boundary_list]
+        peak_monomers = np.hstack((peak_monomers, inds_to_add))
+
+    return peak_monomers.astype(int)
