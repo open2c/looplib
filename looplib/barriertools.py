8f00e9cd306c11abe9e7d64832a0a13040782f74
Hadi Rahmaninejad <rahmanin@discovery2.hpc.usc.edu>
2025-02-09 18:37:43 -0800

    adding a new file to include functions that relies on barriers, including FRiP

--- looplib/barriertools.py
+++ looplib/barriertools.py
@@ -16,3 +16,28 @@ def peak_positions(boundary_list, window_sizes=[1]):
         peak_monomers = np.hstack((peak_monomers, inds_to_add))
 
     return peak_monomers.astype(int)
+
+
+def FRiP(num_sites_t, lef_positions, peak_positions):
+    """
+    Calculate the Fraction of Reads in Peaks (FRiP) score.
+
+    The FRiP score is a measure of how many loop-extruding factor (LEF) positions 
+    fall within predefined peak regions, relative to the total number of LEF positions.
+
+    Parameters:
+    -----------
+    num_sites_t : int
+        Total number of genomic sites.
+    lef_positions : array-like
+        Positions of loop extruding factors (LEFs) along the genome.
+    peak_positions : array-like
+        Indices corresponding to peak regions (CTCFs).
+
+    Returns:
+    --------
+    float
+        The fraction of LEF positions that fall within peak regions.
+    """
+    hist, edges = np.histogram(lef_positions, np.arange(num_sites_t + 1))
+    return np.sum(hist[peak_positions]) / len(lef_positions)
